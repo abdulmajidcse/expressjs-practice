a4d8c0139b9ab23c84f6d634b20b20607d9681b2
Abdul Majid <abdulmajidcse@gmail.com>
2022-07-20 11:52:32 +0600

    404 and 500 error handling.

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.json({
+    statusCode: 200,
+    statusMessage: "Success",
+    data: {
+      message: "Welcome to Express API",
+    },
+  });
+});
+
+app.all("*", function (req, res) {
+  res.status(404);
+  res.json({
+    errorCode: 404,
+    errorMessage: "Not Found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({
+    errorCode: 500,
+    errorMessage: err.message,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`)
+  console.log(`App listening on http://localhost:${port}`);
 });
