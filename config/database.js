b502e1c4c3b9403bfd0e17691d5b7e96788dfc1d
Abdul Majid <abdulmajidcse@gmail.com>
2022-10-04 10:55:16 +0600

    mongodb url lined to env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-database = mongoose.connect("mongodb://localhost:27017/express_practice");
+database = mongoose.connect(process.env.MONGODB_URL);
 
 module.exports = database;
